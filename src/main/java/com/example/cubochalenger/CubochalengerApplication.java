f79889b262f6bc6c7e9de0654a614c073293621f
Lamarkes <lamark12ricarte@gmail.com>
2024-08-13 10:51:58 -0300

    starting project

---
+++ src/main/java/com/example/cubochalenger/CubochalengerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cubochalenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubochalengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubochalengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cubochalenger/CubochalengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cubochalenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubochalengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
